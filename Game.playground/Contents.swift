4c2ef643c80e4741bb90390b621c4e31637020fe
Rodrigo <rodrigofgiglio@gmail.com>
2020-05-08 14:08:14 -0300

    initial commit

---
+++ Game.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+import PlaygroundSupport
+import SpriteKit
+
+// Load the SKScene from 'GameScene.sks'
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+if let scene = GameScene(fileNamed: "GameScene") {
+    // Set the scale mode to scale to fit the window
+    scene.scaleMode = .aspectFill
+    
+    // Present the scene
+    sceneView.presentScene(scene)
+}
+
+PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
