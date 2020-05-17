3e4297f1b10f0958ab074de1a6dd7173015fa2ff
Rodrigo <rodrigofgiglio@gmail.com>
2020-05-16 21:55:13 -0300

    removes first file instructions

--- The Bacteria Adventure.playground/Contents.swift
+++ The Bacteria Adventure.playground/Contents.swift
@@ -2,8 +2,6 @@
 /*:
  
  # The Bacteria Adventure
- 
-This is a maze based game about microbiology. For a smoother experience, turn off the **"Enable Results"** option on the playground app and make it full screen. Enjoy!
- 
+  
  */
 Game.play()
